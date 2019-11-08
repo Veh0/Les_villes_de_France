78fb726e9b25fbf0bb127b42b777d53ff0c7bc0c
veh0 <valentinohleyer@live.fr>
2019-11-08 16:13:49 +0100

    first commit

---
+++ web/sites/default/files/php/twig/5dc584c69f4a8_weather-block.html.twig_XnAcVqDlhD1JSsPYOTFUPcal_/TMAffT4ZQKisE0JVpAzJJnYQsWls3o_hr1xAqW9_O6w.php
@@ -0,0 +1,105 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* sites/all/modules/weather/templates/weather-block.html.twig */
+class __TwigTemplate_b73f14a13827cb3a6fa35538879bb9bc2cc62a1ca5e8cb20045b9a3205cdcbee extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["set" => 11];
+        $filters = ["escape" => 16];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 11
+        $context["classes"] = [0 => "clear_both", 1 => "center"];
+        // line 15
+        echo "
+<div";
+        // line 16
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method")), "html", null, true);
+        echo " style=\"text-align: center\">
+  <img src=\"";
+        // line 17
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["icon"] ?? null)), "html", null, true);
+        echo "\" alt=\"icon\">
+  <p>";
+        // line 18
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["weather"] ?? null)), "html", null, true);
+        echo "</p>
+  <p><b>";
+        // line 19
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["temperature"] ?? null)), "html", null, true);
+        echo "°C</b></p>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "sites/all/modules/weather/templates/weather-block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  72 => 19,  68 => 18,  64 => 17,  60 => 16,  57 => 15,  55 => 11,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "sites/all/modules/weather/templates/weather-block.html.twig", "C:\\wamp64\\www\\eval-d8\\web\\sites\\all\\modules\\weather\\templates\\weather-block.html.twig");
+    }
+}
